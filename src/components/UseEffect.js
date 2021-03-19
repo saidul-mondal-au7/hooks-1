844c3f20f9d3a00aace72a9a0776b2ab594ff4bf
Saidul Mondal <mondalsaidul3232@gmail.com>
2021-03-19 10:02:01 +0530

    updated

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -3,14 +3,17 @@ import React, { useState, useEffect } from 'react'
 function UseEffect() {
     const initialState = 0;
     const [counter, increaseCounter] = useState(initialState)
+    const [name, setName] = useState("")
     
 useEffect(() => {
+    console.log("updating document title...")
     document.title = `You have clicked ${counter} times.`
-})
+},[counter])
 
     return (
         <div>
            <h1>{counter}</h1>
+           <input type="text" value={name} onChange={(e)=> setName(e.target.value)} />
            <button onClick={()=>increaseCounter(counter+1)}>useEffect</button>
         </div>
     )
--- src/components/UseEffect1.js
+++ src/components/UseEffect1.js
@@ -3,19 +3,25 @@ import React, { Component } from 'react'
 
 export default class UseEffect1 extends Component {
    state = {
-       counter: 0
+       counter: 0,
+       name : ""
    }
    componentDidMount(){
        document.title = `You have clicked ${this.state.counter} times.`
    }
-   componentDidUpdate(){
-    document.title = `You have clicked ${this.state.counter} times.`
+   componentDidUpdate(prevProps, prevState){
+       if(prevState.counter !== this.state.counter){
+        console.log("updating document title...")
+        document.title = `You have clicked ${this.state.counter} times.`
+       }
+       
    }
 
     render() {
         return (
             <div>
                <h1>{this.state.counter}</h1> 
+               <input type='text' value={this.state.name} onChange={(e)=> this.setState({ name : e.target.value })} />
                <button onClick={()=>this.setState({counter: this.state.counter +1 })}>useEffect1</button>
             </div>
         )
